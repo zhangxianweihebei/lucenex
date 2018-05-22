cbe916ba5934e47b8e25feb54483c775e2917bc5
zhangxianwei <zhangxianwei@nnthink.com>
2018-05-22 11:15:24 +0800

    初始化提交

---
+++ src/main/java/com/ld/lucenex/App.java
@@ -0,0 +1,32 @@
+/**  
+ * Copyright © 2018LD. All rights reserved.
+ *
+ * @Title: App.java
+ * @Prject: lucenex
+ * @Package: com.ld.lucenex
+ * @Description: TODO
+ * @author: Myzhang  
+ * @date: 2018年5月22日 上午11:00:57
+ * @version: V1.0  
+ */
+package com.ld.lucenex;
+
+/**
+ * @ClassName: App
+ * @Description: TODO
+ * @author: Myzhang  
+ * @date: 2018年5月22日 上午11:00:57
+ */
+public class App {
+	
+	/**
+	 * @Title: main
+	 * @Description: TODO
+	 * @param args
+	 * @return: void
+	 */
+	public static void main(String[] args) {
+		System.out.println("世界你好!");
+	}
+
+}
