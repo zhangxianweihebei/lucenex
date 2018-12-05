7715d5a722385a55c90d26a1536b14f5c3891f0f
ld <523812162@qq.com>
2018-12-05 20:27:57 +0800

    ld
    代码缺陷修改

--- src/main/java/com/ld/lucenex/core/LuceneX.java
+++ src/main/java/com/ld/lucenex/core/LuceneX.java
@@ -2,8 +2,11 @@ package com.ld.lucenex.core;
 
 import com.ld.lucenex.base.BaseConfig;
 import com.ld.lucenex.config.LuceneXConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class LuceneX {
+	private static Logger logger = LoggerFactory.getLogger(LuceneX.class);
 
 	private static final LuceneX luceneX = new LuceneX();
 	private LuceneX() {
@@ -19,7 +22,7 @@ public class LuceneX {
 			LuceneXConfig config = (LuceneXConfig) clas.newInstance();
 			BaseConfig.configLuceneX(config);
 		} catch (InstantiationException | IllegalAccessException e) {
-			e.printStackTrace();
+			logger.error("lucenex start error",e);
 		}
 	}
 }
