2fe78c2f67f47171e18991faf55c0aad9fde0183
ld <523812162@qq.com>
2019-01-18 01:19:54 +0800

    Revert "同步"
    This reverts commit 453525bdcfdf2b8d84d56ed1a885d1cafd87564a.

--- src/main/java/com/ld/lucenex/interce/impl/NRTIterface.java
+++ src/main/java/com/ld/lucenex/interce/impl/NRTIterface.java
@@ -46,15 +46,17 @@ public class NRTIterface implements LdInterface {
         if (methodName.indexOf("add") != -1 || methodName.indexOf("save") != -1
                 || methodName.indexOf("update") != -1 || methodName.indexOf("del") != -1) {
             SourceConfig dataSource = ManySource.getDataSource();
-            try {
-                dataSource.restartReader();
-                if (BaseConfig.baseConfig().isDevMode()) {
-                    dataSource.getWriter().commit();
+            CompletableFuture.runAsync(() -> {
+                try {
+                    dataSource.restartReader();
+                    if (BaseConfig.baseConfig().isDevMode()) {
+                        dataSource.getWriter().commit();
+                    }
+                    logger.info("NRTIterface->afterMethod success");
+                } catch (IOException e) {
+                    logger.error("NRTIterface->afterMethod error", e);
                 }
-                logger.info("NRTIterface->afterMethod success");
-            } catch (IOException e) {
-                logger.error("NRTIterface->afterMethod error", e);
-            }
+            });
         }
         return true;
     }
